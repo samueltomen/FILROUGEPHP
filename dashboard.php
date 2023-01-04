6d21886bac206cbcd8abe6fa243dfd40c1fad39e
Dytsaw <tomennana.samuel@outlook.fr>
2023-01-04 17:37:44 +0100

    modification du dashboard

--- dashboard.php
+++ dashboard.php
@@ -12,13 +12,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/Main.css" />
     <script defer src="./Animation.js"></script>
+    <script defer src="api.js"></script>
 
 </head>
 
 <body>
     <main>
         <?php include('navBar.php') ?>
-        <h1>Bienvenue sur votre Dashboard</h1>
+        <h1 class="text-center my-5">Bienvenue sur votre Dashboard 🖐</h1>
+        <br>
+        <div class="informations d-flex justify-content-evenly">
+            <div class="dons text-center">
+                <h3 class="font-weight-italic text-decoration-underline">VOS DONS</h3>
+                <div id="donations-widget">
+                    <h4>Dons fait cette année</h4>
+                    <p id="donation-count">0</p>
+                </div>
+            </div>
+            <div class="vaccin text-center">
+                <h3>VACCINS</h3>
+            </div>
+        </div>
+        <!-- API -->
+
     </main>
     <?php include('./footer.php') ?>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
